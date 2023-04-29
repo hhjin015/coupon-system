96e0a5fa04ea8af014dd94bb4735b04649f4bf4f
hyejin <heo015414@gmail.com>
2023-04-30 03:16:27 +0900

    initial commit

--- practice/src/main/java/com/github/hhjin015/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.github.hhjin015;
-
-public class HelloService {
-    public String helloWorld() {
-        return "hello world";
-    }
-}
--- server/src/main/java/com/github/hhjin015/coupons/HelloController.java
+++ server/src/main/java/com/github/hhjin015/coupons/HelloController.java
@@ -1,12 +1,10 @@
 package com.github.hhjin015.coupons;
 
-import com.github.hhjin015.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    private HelloService service;
 
     @GetMapping("/hello")
     public String hello() {
