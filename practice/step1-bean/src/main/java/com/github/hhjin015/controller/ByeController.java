9c27c822f004dd2aa063c2b58654a7a0742c4e08
hyejin <heo015414@gmail.com>
2023-05-01 22:23:52 +0900

    delete practice

--- practice/step1-bean/src/main/java/com/github/hhjin015/controller/ByeController.java
+++
@@ -1,7 +0,0 @@
-package com.github.hhjin015.controller;
-
-public class ByeController {
-    public String bye(String name) {
-        return name + ", farewell to spring";
-    }
-}
--- practice/step1-bean/src/main/java/com/github/hhjin015/controller/HelloController.java
+++
@@ -1,7 +0,0 @@
-package com.github.hhjin015.controller;
-
-public class HelloController {
-    public String hello(String name) {
-        return name + ", welcome to spring";
-    }
-}
